3875c75a72c646107996e3ae36f0beb8f023778f
RaukampMika <mika.raukamp@buerokompetenz.de>
2022-06-25 15:34:30 +0200

    Added Dark-Mode

--- DrumPad/MainWindow.xaml.cs
+++ DrumPad/MainWindow.xaml.cs
@@ -5,6 +5,10 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using WinRT;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Storage;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -24,9 +28,71 @@ namespace DrumPad
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        AppWindow appWindow;
         public MainWindow()
         {
+            appWindow = GetAppWindow();
             this.InitializeComponent();
+        }
+
+        public void ToggleSwitch_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ApplicationData.Current.LocalSettings.Values.TryGetValue("themeSetting", out object themeSetting) && (int)themeSetting == 0)
+            {
+                dark_switch.IsOn = true;
+            }
+            else
+            {
+                dark_switch.IsOn = false;
+            }
+        }
+
+        public void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    if (this.Content is FrameworkElement frameworkElement)
+                    {
+                        ((FrameworkElement) this.Content).RequestedTheme = ElementTheme.Dark;
+                    }
+                }
+                else
+                {
+                    if (this.Content is FrameworkElement frameworkElement)
+                    {
+                        ((FrameworkElement) this.Content).RequestedTheme = ElementTheme.Light;
+                    }
+                }
+
+            }
+            ApplicationData.Current.LocalSettings.Values["themeSetting"] = ((ToggleSwitch)sender).IsOn ? 0 : 1;
+        }
+
+        private AppWindow GetAppWindow()
+        {
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId myWndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(myWndId);
+        }
+
+        private void SwtichPresenter_Overlay(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem dropDownButton = sender as MenuFlyoutItem;
+            if (dropDownButton.Name == "CompOverlay")
+            {
+                appWindow.SetPresenter(AppWindowPresenterKind.CompactOverlay);
+            }
+            else if (dropDownButton.Name == "Fullscreen")
+            {
+                appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+            }
+            else
+            {
+                appWindow.SetPresenter(AppWindowPresenterKind.Default);
+            }
         }
 
         #region Event handlers buttons
