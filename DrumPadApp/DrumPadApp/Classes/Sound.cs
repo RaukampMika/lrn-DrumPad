141348debf953f8e1dc642c61270acfc441bf904
RaukampMika <mika.raukamp@buerokompetenz.de>
2022-06-26 02:00:46 +0200

    Switch auf neus Project Template

--- DrumPadApp/DrumPadApp/Classes/Sound.cs
+++ DrumPadApp/DrumPadApp/Classes/Sound.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Media;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Media;
-using System.IO;
 
-namespace DrumPad
+namespace DrumPadApp
 {
     class Sound
     {
         public Sound(string soundName)
         {
             var soundFile = Path.Join(Windows.ApplicationModel.Package.Current.InstalledLocation.Path, "Assets/Sounds", soundName);
-            SoundPlayer player = new System.Media.SoundPlayer(soundFile);
+            SoundPlayer player = new SoundPlayer(soundFile);
             player.Play();
         }
     }
--- DrumPadApp/DrumPadApp/MainWindow.xaml.cs
+++ DrumPadApp/DrumPadApp/MainWindow.xaml.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using WinRT;
-using Microsoft.UI;
-using Microsoft.UI.Windowing;
-using Windows.Storage;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -16,12 +14,13 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using System.Media;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace DrumPad
+namespace DrumPadApp
 {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
@@ -56,14 +55,14 @@ namespace DrumPad
                 {
                     if (this.Content is FrameworkElement frameworkElement)
                     {
-                        ((FrameworkElement) this.Content).RequestedTheme = ElementTheme.Dark;
+                        ((FrameworkElement)this.Content).RequestedTheme = ElementTheme.Dark;
                     }
                 }
                 else
                 {
                     if (this.Content is FrameworkElement frameworkElement)
                     {
-                        ((FrameworkElement) this.Content).RequestedTheme = ElementTheme.Light;
+                        ((FrameworkElement)this.Content).RequestedTheme = ElementTheme.Light;
                     }
                 }
 
